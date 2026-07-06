0da695f5673300332805c89e552d393c98f3c6bb
SUKUMAR GOPE <138880865+Sg-2003@users.noreply.github.com>
2026-07-06 22:02:58 +0530

    Support dynamic avatar URL in Navbar profile image

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,7 +24,17 @@ const Navbar = () => {
         <div className="navbar-right">
           {userData ? (
             <div className="user-menu">
-              <img src={assets.profile_img} alt="Profile" className="profile-avatar" />
+              <img
+                src={
+                  userData?.image
+                    ? userData.image.startsWith("http")
+                      ? userData.image
+                      : `${backendUrl}/uploads/${userData.image}`
+                    : assets.profile_img
+                }
+                alt="Profile"
+                className="profile-avatar"
+              />
               <div className="user-dropdown">
                 <button onClick={() => navigate("/applications")}>Applied Jobs</button>
                 <button onClick={handleLogout}>Logout</button>
